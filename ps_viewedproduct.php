8c8dba8f60a004ffef299b7cfca1d45de39ef576
Thomas Nabord <thomas.nabord@prestashop.com>
2018-12-03 09:23:34 +0000

    Bump version to 1.2.0

--- ps_viewedproduct.php
+++ ps_viewedproduct.php
@@ -43,7 +43,7 @@ class Ps_Viewedproduct extends Module implements WidgetInterface
     {
         $this->name = 'ps_viewedproduct';
         $this->author = 'PrestaShop';
-        $this->version = '1.1.0';
+        $this->version = '1.2.0';
         $this->tab = 'front_office_features';
         $this->need_instance = 0;
 
