2d73f39bc372c84abac177af0a59c0b65c7639a6
Thomas Nabord <thomas.nabord@prestashop.com>
2019-07-01 09:15:26 +0100

    Uninstall module used on PS 1.6 before using this one

--- ps_viewedproduct.php
+++ ps_viewedproduct.php
@@ -36,6 +36,11 @@ if (!defined('_PS_VERSION_')) {
 
 class Ps_Viewedproduct extends Module implements WidgetInterface
 {
+    /**
+     * @var string Name of the module running on PS 1.6.x. Used for data migration.
+     */
+    const PS_16_EQUIVALENT_MODULE = 'blockviewed';
+    
     private $templateFile;
     private $currentProductId;
 
@@ -67,14 +72,38 @@ class Ps_Viewedproduct extends Module implements WidgetInterface
 
     public function install()
     {
+        $this->uninstallPrestaShop16Module();
+        if (!Configuration::get('PRODUCTS_VIEWED_NBR')) {
+            Configuration::updateValue('PRODUCTS_VIEWED_NBR', 8);
+        }
         return parent::install()
-            && Configuration::updateValue('PRODUCTS_VIEWED_NBR', 8)
             && $this->registerHook('displayFooterProduct')
             && $this->registerHook('displayProductButtons') //Former version of displayProductAdditionalInfo
             && $this->registerHook('displayProductAdditionalInfo')
             && $this->registerHook('actionObjectProductDeleteAfter')
             && $this->registerHook('actionObjectProductUpdateAfter')
         ;
+    }
+
+    /**
+     * Migrate data from 1.6 equivalent module (if applicable), then uninstall
+     */
+    public function uninstallPrestaShop16Module()
+    {
+        if (!Module::isInstalled(self::PS_16_EQUIVALENT_MODULE)) {
+            return false;
+        }
+        $oldModule = Module::getInstanceByName(self::PS_16_EQUIVALENT_MODULE);
+        if ($oldModule) {
+            // This closure calls the parent class to prevent data to be erased
+            // It allows the new module to be configured without migration
+            $parentUninstallClosure = function() {
+                return parent::uninstall();
+            };
+            $parentUninstallClosure = $parentUninstallClosure->bindTo($oldModule, get_class($oldModule));
+            $parentUninstallClosure();
+        }
+        return true;
     }
 
     public function hookActionObjectProductDeleteAfter($params)
